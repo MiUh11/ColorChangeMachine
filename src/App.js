2f71e27241822cfc01e125d4e420019616c6ea38
Michal Uher <Michal.Uher.24@gmail.com>
2020-10-15 16:00:51 +0200

    prvý commit pridanie App.js

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useReducer } from "react";
+import "./App.css";
+
+import { useWindowSize } from "react-use";
+import classNames from "classnames";
+
+import Box from "./Box";
+
+const THEMES = {
+  LIGHT: "LIGHT",
+  DARK: "DARK",
+  RED: "RED",
+  GREEN: "GREEN",
+  BLUE: "BLUE",
+};
+
+const DEFAULT_THEME = {
+  key: THEMES.LIGHT,
+};
+const reducer = (currentState, action) => {
+  console.log("reducer currentState", currentState);
+  console.log("reducer action", action);
+  return { key: action.nextTheme };
+};
+
+export default function App() {
+  const [currentState, dispatch] = useReducer(reducer, DEFAULT_THEME);
+
+  const { width, height } = useWindowSize();
+
+  console.log("App currentState", currentState);
+  console.log("width", width);
+  console.log("height", height);
+
+  const boxDimension = width / 4;
+
+  return (
+    <div
+      id="container"
+      className={classNames({
+        "application-light": currentState.key === THEMES.LIGHT,
+        "application-dark": currentState.key === THEMES.DARK,
+        "application-red": currentState.key === THEMES.RED,
+        "application-green": currentState.key === THEMES.GREEN,
+        "application-blue": currentState.key === THEMES.BLUE,
+      })}
+    >
+      <h1>Color Changing Machine</h1>
+      <div className="boxes-wrapper">
+        <Box boxDimension={boxDimension} />
+        <Box boxDimension={boxDimension} />
+        <Box boxDimension={boxDimension} />
+      </div>
+      <div className="buttons">
+        <button
+          className="default-button"
+          onClick={() => dispatch({ nextTheme: THEMES.LIGHT })}
+        >
+          Default
+        </button>
+        <button
+          className="dark-button"
+          onClick={() => dispatch({ nextTheme: THEMES.DARK })}
+        >
+          Dark
+        </button>
+        <button
+          className="red-button"
+          onClick={() => dispatch({ nextTheme: THEMES.RED })}
+        >
+          Red
+        </button>
+        <button
+          className="green-button"
+          onClick={() => dispatch({ nextTheme: THEMES.GREEN })}
+        >
+          Green
+        </button>
+        <button
+          className="blue-button"
+          onClick={() => dispatch({ nextTheme: THEMES.BLUE })}
+        >
+          Blue
+        </button>
+      </div>
+    </div>
+  );
+}
